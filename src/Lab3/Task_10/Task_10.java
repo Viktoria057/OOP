a9bcc27c94cc2909d6ce9fc81296f2d08f97e6e8
VikaSobolevska <sobolevskaaviktoria805@gmail.com>
2025-03-20 13:49:57 +0200

    first commit

--- src/Lab3/Task_10/Task_10.java
+++ src/Lab3/Task_10/Task_10.java
@@ -1,4 +1,34 @@
 package Lab3.Task_10;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Task_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter the array size: ");
+        int n = scanner.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter an array а[" + i + "]  element: ");
+            a[i] = scanner.nextInt();
+        }
+        System.out.println("Massif: " + Arrays.toString(a));
+        System.out.print("All duplicates in an array: ");
+
+        for (int i = 0; i < n-1; i++){
+            for (int j = i + 1; j < n; j++){
+                if(a[i] == a[j]){
+                    System.out.print(a[i] + " ");
+                    break;
+                }
+
+            }
+        }
+    }
 }
--- src/Lab3/Task_9/Task_9.java
+++ src/Lab3/Task_9/Task_9.java
@@ -1,4 +1,31 @@
 package Lab3.Task_9;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter the array size: ");
+        int n = scanner.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = random.nextInt(0, 100);
+        }
+        System.out.println("Massif: " + Arrays.toString(a));
+
+        System.out.println("What index of the element will you find?");
+        int j = scanner.nextInt();
+        System.out.println("What is the new meaning?");
+        a[j] = scanner.nextInt();
+
+        System.out.println("Massif: " + Arrays.toString(a));
+
+        scanner.close();
+    }
 }
+
